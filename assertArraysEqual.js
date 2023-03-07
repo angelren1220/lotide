9d50ab577ca02e0b4f847a5b74e2c0d1b111263a
Linuxe <angelren1220@gmail.com>
2023-03-07 16:57:20 -0500

    refined code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,23 @@
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
   }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+
 };
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+    console.log(`✅✅✅Assertion Passed: ${JSON.stringify(arr1)} === ${JSON.stringify(arr2)}`);
+    return;
   }
-  if (!eqArrays(arr1, arr2)) {
-    console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`);
-  }
+  console.log(`🛑🛑🛑Assertion Failed: ${JSON.stringify(arr1)} !== ${JSON.stringify(arr2)}`);
+
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
@@ -25,3 +25,4 @@ assertArraysEqual([1, 2, 3], [3, 2, 1]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
 
+console.log(JSON.stringify(["1", "2", 3]));
