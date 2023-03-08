9c1d8e2932afe72514a91abc2f613bbe4253f8f4
Linuxe <angelren1220@gmail.com>
2023-03-08 12:01:34 -0500

    count the letters in a string

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+  console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function(string) {
+  const letterCounts = {};
+  const lowerCaseStr = string.toLowerCase();
+  for (let letter of lowerCaseStr) {
+    if (letterCounts[letter]) {
+      letterCounts[letter] += 1;
+      continue;
+    }
+    letterCounts[letter] = 1;
+  }
+  console.log(letterCounts);
+  return letterCounts;
+};
+
+// test case
+assertEqual(countLetters("LHL").l, 2);
+assertEqual(countLetters("angelren").e, 2);
+assertEqual(countLetters("AngelLinxueRen").l, 2);
