55b185fc9a570937e560c33f478bb9563abd53fd
Linuxe <angelren1220@gmail.com>
2023-03-09 16:31:02 -0500

    test case using assertArraysEqual

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,25 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅Assertion Passed: ${JSON.stringify(arr1)} === ${JSON.stringify(arr2)}`);
+    return;
+  }
+  console.log(`🛑🛑🛑Assertion Failed: ${JSON.stringify(arr1)} !== ${JSON.stringify(arr2)}`);
+
+};
+
 const takeUntil = function(array, callback) {
   const result = [];
   for (let item of array) {
@@ -12,10 +34,8 @@ const takeUntil = function(array, callback) {
 // test case
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
