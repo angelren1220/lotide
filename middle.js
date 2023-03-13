d481b8d0a4b07ecc2b89d265ab0299b4c6bdef76
Linuxe <angelren1220@gmail.com>
2023-03-13 15:47:52 -0400

    middle refactored and test

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅Assertion Passed: ${JSON.stringify(arr1)} === ${JSON.stringify(arr2)}`);
-    return;
-  }
-  console.log(`🛑🛑🛑Assertion Failed: ${JSON.stringify(arr1)} !== ${JSON.stringify(arr2)}`);
-
-};
-
-// ACTUAL FUNCTION
 const middle = function(array) {
   if (array.length > 2) {
     if (array.length % 2 === 0) {
@@ -32,10 +8,4 @@ const middle = function(array) {
   return [];
 };
 
-// TEST CODE
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([]), []);
+module.exports = middle;
